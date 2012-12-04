dd88047040dce4a926a6ab80e207bc757f44d648
vasyl.yushchyshen <vasyl.yushchyshen@ti.com>
2012-12-04 21:12:21 +0200

    OMAP4: Fix for build error "no support ARM mode `smc #0'"
    Since 4.5 GCC version assembler requires -march=armv7-a+sec
    comand line parameter to enable the smc instruction using.
    This patch is similar to kernel one. It uses as-instr to check if
    mentioned comand line parameter should be used.
    
    modified:   cpu/omap4/config.mk
    
    Change-Id: Ied1d070867af2e087e88972a6e3be07a603fe3a0
    Signed-off-by: vasyl.yushchyshen <vasyl.yushchyshen@ti.com>

--- cpu/omap4/config.mk
+++ cpu/omap4/config.mk
@@ -28,5 +28,30 @@ PLATFORM_CPPFLAGS += -march=armv7-a
 # Supply options according to compiler version
 #
 # =========================================================================
+# gcc support functions
+# See Linux kernel documentation in Documentation/kbuild/makefiles.txt
+
+# try-run
+# Usage: option = $(call try-run, $(CC)...-o "$$TMP",option-ok,otherwise)
+# Exit code chooses option. "$$TMP" is can be used as temporary file and
+# is automatically cleaned up.
+try-run = $(shell set -e;		\
+	TMP="$(TMPOUT).$$$$.tmp";	\
+	TMPO="$(TMPOUT).$$$$.o";	\
+	if ($(1)) >/dev/null 2>&1;	\
+	then echo "$(2)";		\
+	else echo "$(3)";		\
+	fi;				\
+	rm -f "$$TMP" "$$TMPO")
+
+# as-instr
+# Usage: cflags-y += $(call as-instr,instr,option1,option2)
+
+as-instr = $(call try-run,\
+	printf "%b \n" "$(1)" | $(CC) $(AFLAGS) -c -x assembler -o "$$TMP" -,$(2),$(3))
+
+plus_sec := $(call as-instr,.arch_extension sec,+sec)
+
+PLATFORM_CPPFLAGS +=-Wa,-march=armv7-a$(plus_sec)
 PLATFORM_CPPFLAGS +=$(call cc-option,-mapcs-32,-mabi=apcs-gnu)
 PLATFORM_RELFLAGS +=$(call cc-option,-mshort-load-bytes,$(call cc-option,-malignment-traps,))
